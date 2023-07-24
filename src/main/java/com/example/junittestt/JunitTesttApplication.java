46eae712a41bb797b74b18d41f5c251b59dcc944
Asal <asal.salmani@yahoo.com>
2023-07-24 08:48:25 +0330

    added the files

---
+++ src/main/java/com/example/junittestt/JunitTesttApplication.java
@@ -0,0 +1,13 @@
+package com.example.junittestt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTesttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTesttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/junittestt/JunitTesttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junittestt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTesttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
